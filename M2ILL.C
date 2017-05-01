9bc2d880c77b02f3592001d44dbf067de8a3102b
root <root@localhost>
2017-05-01 19:06:35 +0800

    recore

---
+++ M2ILL.C
@@ -0,0 +1,13 @@
+// Merge two incremental linked lists.
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+	}node;
+	
+struct node *createLinkedListIncremental(){
+	//NOTICE: Linked list here have no leading 
+	
+	}
