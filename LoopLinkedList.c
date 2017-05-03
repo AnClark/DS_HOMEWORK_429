4c73e165c2f70018f4894fe1102ec2746a8439a7
root <root@localhost>
2017-05-03 21:11:11 +0800

    Continue! Yeah!

--- LoopLinkedList.c
+++ LoopLinkedList.c
@@ -39,6 +39,23 @@ struct node *createLinkedList()
 
 }
 
+
+void printLinkedList(struct node *head)
+{
+	struct node *p, *first;
+	first = head->next;
+	p = first->next;
+
+	puts("----------- LINKED LIST IS -----------");
+	while (p != first)
+	{
+		printf("%d  ", p->data);
+		p = p->next;
+	}
+	puts("\n---------------------------------");
+}
+
+
 int countLoopedLinkedListNodes(struct node *head)
 {
 	struct node *p, *first;
@@ -56,6 +73,40 @@ int countLoopedLinkedListNodes(struct node *head)
 	return count;
 }
 
+
+void removeNodeByPosition(struct node *head, int position)
+{
+	struct node *p, *first, *q;
+	int i = 1;
+
+	// NOTICE: Position starts from 1 here.
+	// Check if input position value invalid
+	int len = countLoopedLinkedListNodes(head);
+	if (position < 0 || position > len)
+	{
+		puts("*** FATAL: Invalid position! ***");
+		return;
+	}
+
+	// Initialize
+	first = head->next;
+	p = first;
+
+	// Find out the target node
+	while (p->next != first && i < position)
+	{
+		p = p->next;
+		i++;
+	}
+
+	// Remove
+	q = p->next;
+	p->next = q->next;
+	free(q);
+
+}
+
+#define TEST_REMOVE_BY_POSITION
 int main()
 {
 	struct node *head;
@@ -63,7 +114,27 @@ int main()
 	puts("Now input linked list nodes.");
 	head = createLinkedList();
 
+	puts("Now print out what you have constructed.");
+	printLinkedList(head);
+
 	puts("Let's count how many nodes in your list.");
 	printf("\tCount: %d\n", countLoopedLinkedListNodes(head));
-}
 
+
+#ifdef TEST_REMOVE_BY_POSITION
+	int p;
+	puts("\nNow test removing nodes by position. Position starts from 1. 0 for EOL.");
+	while (scanf("%d", &p) && p != 0)
+	{
+		if (countLoopedLinkedListNodes(head) <= 0)
+		{
+			puts("--- Linked list is empty now ---");
+			break;
+		}
+
+		removeNodeByPosition(head, p);
+		printLinkedList(head);
+	}
+#endif
+
+}
